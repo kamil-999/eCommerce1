20684e9ceb7d73825c4fd541df88cb5976acea2d
kamil-999 <Abdikafihersi18@gmail.com>
2026-07-20 10:29:40 +0300

    starter

--- eCommerceApp/urls.py
+++ eCommerceApp/urls.py
@@ -5,5 +5,13 @@ from eCommerceApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('chefs/', views.chefs, name='chefs'),
+    path('contact/', views.contact, name='contact'),
+    path('menu/', views.menu, name='menu'),
+    path('orders/', views.orders, name='orders'),
+    path('reservation/', views.reservation, name='reservation'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('starter_page/', views.starter_page, name='starter_page'),
 ]
--- eCommerceApp/views.py
+++ eCommerceApp/views.py
@@ -1,5 +1,23 @@
+from urllib import request
+
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def chefs (request):
+    return render(request, 'chefs.html')    
+def contact(request):
+    return render(request, 'contact.html')
+def menu(request):
+    return render(request, 'menu.html')
+def orders(request):
+    return render(request, 'orders.html')   
+def reservation(request):
+    return render(request, 'reservation.html')
+def reviews(request):
+    return render(request, 'reviews.html')  
+def starter_page(request):
+    return render(request, 'starter_page.html') 
